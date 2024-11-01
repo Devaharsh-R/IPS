3beca17aabc113c950911a31fde90a558967b3f8
Devaharsh R <devaharsh14@gmail.com>
2024-11-01 16:36:37 +0530

    - a.py is renamed to refresh.py - done the conversion of html code to data list without empty data

--- Application/python/refresh.py
+++ Application/python/refresh.py
@@ -1,8 +1,10 @@
-# bsnl data retreval.
 import requests as re
 from bs4 import BeautifulSoup
 response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html')
+# This is bsnl retreival only
 op=response.text
-content=BeautifulSoup(op).text
+content=BeautifulSoup(op,'html.parser').text
 words=content.split('\n')
+words = list(filter(None, words))
 print(words)
+# TODO Remove unwanted data from the list
