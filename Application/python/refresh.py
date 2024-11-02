bc8ba62b27b50f600075cd7a7b1e610ae129d4f7
Devaharsh R <devaharsh14@gmail.com>
2024-11-02 07:13:35 +0530

    taking the cost only is not completed

--- Application/python/refresh.py
+++ Application/python/refresh.py
@@ -5,5 +5,11 @@ response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepai
 html=response.text
 content=BeautifulSoup(html,'html.parser')#html code
 cost=content.select('tr.table_conn')[0].text
+cost=cost.split('\n')
+cost = list(filter(None, cost))
+cost = [el.replace('\xa0',' ') for el in cost]
+for value in cost:
+    if not value.isdigit():
+        cost.remove(value) #FIXME some wanted values are removed
 print(cost)
-# TODO Remove unwanted data from the list
+# TODO Take all the wanted data from the
