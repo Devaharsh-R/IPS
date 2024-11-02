8e588be807b9a3a01739b17d8d37c6d4bb32a004
Devaharsh R <devaharsh14@gmail.com>
2024-11-02 08:34:40 +0530

    successfully retrieved the data perfectly (cost only)

--- Application/python/refresh.py
+++ Application/python/refresh.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 # This is bsnl retreival only
 response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html')
 html=response.text
-content=BeautifulSoup(html,'html.parser')#html code
-cost=content.select('tr.table_conn')[0].text
+html=BeautifulSoup(html,'html.parser')#html code
+cost=html.select('tr.table_conn')[0].text
 cost=cost.split('\n')
 cost = list(filter(None, cost))
-cost = [el.replace('\xa0',' ') for el in cost]
+cost = [el.replace('\xa0','') for el in cost] #reomoving uncode
+cost = [el.replace(' ','') for el in cost]#The list become cost only
 for value in cost:
     if not value.isdigit():
-        cost.remove(value) #FIXME some wanted values are removed
+        cost.remove(value)
+print(cost.__len__())
 print(cost)
-# TODO Take all the wanted data from the
+# TODO Take all the wanted data from the site
