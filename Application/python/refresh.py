249a51d0752d2ffdbfe9a2194c51b730c5fce0b2
Devaharsh R <devaharsh14@gmail.com>
2024-11-02 06:24:45 +0530

    got a perfect filter can take cost only to variable

--- Application/python/refresh.py
+++ Application/python/refresh.py
@@ -1,10 +1,9 @@
 import requests as re
 from bs4 import BeautifulSoup
-response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html')
 # This is bsnl retreival only
-op=response.text
-content=BeautifulSoup(op,'html.parser').text
-words=content.split('\n')
-words = list(filter(None, words))
-print(words)
+response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html')
+html=response.text
+content=BeautifulSoup(html,'html.parser')#html code
+cost=content.select('tr.table_conn')[0].text
+print(cost)
 # TODO Remove unwanted data from the list
