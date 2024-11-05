d50d6e0e50fcc475cd83a598f1572851fba8a365
Devaharsh R <devaharsh14@gmail.com>
2024-11-05 16:20:22 +0530

    task only branch created

--- Application/php/my/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include("simple_html_dom.php");
-$removedTags = file_get_html("https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html");
-// $content=htmlspecialchars($html);
-// $removedTags="<H1>HELLO WORLD</H1><BR><U>HI</U>";
-$removedTags=filter_var($removedTags,FILTER_SANITIZE_STRING);
-echo $removedTags;
-?>
--- Application/python/a.py
+++
@@ -1,8 +0,0 @@
-# bsnl data retreval.
-import requests as re
-from bs4 import BeautifulSoup
-response=re.get('https://www.bsnl.co.in/opencms/bsnl/BSNL/services/mobile/prepaid_plans_100822.html')
-op=response.text
-content=BeautifulSoup(op).text
-words=content.split('\n')
-print(words)
