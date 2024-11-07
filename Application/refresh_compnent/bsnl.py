a25cf748f646ccffe383c68288eed40dbc734f4f
Devaharsh R <devaharsh14@gmail.com>
2024-11-07 21:12:40 +0530

    regex to work

--- Application/refresh_compnent/bsnl.py
+++ Application/refresh_compnent/bsnl.py
@@ -1,4 +1,5 @@
-from refresh_compnent import accessKey as key
+# from refresh_compnent import accessKey as key
+import re #regex
 
 def bsnl(data):
     filteredContent={}
@@ -9,7 +10,10 @@ def bsnl(data):
                 temp.append(value) # inserting cost value to list
     filteredContent['cost']=temp # inserted the list
     # RE for plan: ('Plan'+'FRC').'-'.(0-9)*
-    temp=[] # empty list for plan
-    #TODO PLAN data filter
+    temp=[] # empty list for 
+    pattern=r'(Plan|FRC)-\d*'
+    for value in data['heading']:
+         temp=re.findall(pattern,value)
+    #FIXME PLAN data filter
     filteredContent['plan']=temp
     return filteredContent
